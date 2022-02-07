e5250031bc76d2ccd14dfa8d54fe616c08a3efa6
Piyush Jha <piyush.jha@uwaterloo.ca>
2022-02-07 10:04:10 -0500

    added support for multiple Preference Constraints

--- src/checkers/inference/solver/backend/geneticmaxsat/GeneticMaxSatSolver.java
+++ src/checkers/inference/solver/backend/geneticmaxsat/GeneticMaxSatSolver.java
@@ -7,43 +7,33 @@ import checkers.inference.solver.backend.maxsat.MaxSatSolver;
 import checkers.inference.solver.frontend.Lattice;
 import checkers.inference.solver.util.FileUtils;
 import checkers.inference.solver.util.SolverEnvironment;
-import io.jenetics.IntegerGene;
-import io.jenetics.MeanAlterer;
-import io.jenetics.Mutator;
-import io.jenetics.Optimize;
-import io.jenetics.RouletteWheelSelector;
-import io.jenetics.TournamentSelector;
-import io.jenetics.engine.Codecs;
-import io.jenetics.engine.Engine;
-import io.jenetics.engine.EvolutionResult;
-import io.jenetics.engine.EvolutionStatistics;
-import io.jenetics.util.IntRange;
-import org.sat4j.maxsat.WeightedMaxSatDecorator;
-import org.sat4j.maxsat.reader.WDimacsReader;
-import org.sat4j.pb.IPBSolver;
-import org.sat4j.reader.ParseFormatException;
-import org.sat4j.specs.ContradictionException;
-import org.sat4j.specs.TimeoutException;
+import org.checkerframework.javacutil.BugInCF;
+import org.plumelib.util.Pair;
+import org.sat4j.core.VecInt;
 
 import javax.lang.model.element.AnnotationMirror;
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import static io.jenetics.engine.EvolutionResult.toBestEvolutionResult;
-import static io.jenetics.engine.Limits.bySteadyFitness;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class GeneticMaxSatSolver extends MaxSatSolver {
 
-    private String wcnfFileContent;
+    public int allSoftWeightsCount = 0;
+    public long allSoftWeightsSum = 0;
+    public List<Integer> oldSoftWeights;
+    public List<Pair<Integer, Integer>> softWeightsCounter;
+    public long uniqueSoftWeightsCount = 0;
+    public String wcnfFileContent;
 
     public GeneticMaxSatSolver(SolverEnvironment solverEnvironment, Collection<Slot> slots,
                                Collection<Constraint> constraints, MaxSatFormatTranslator formatTranslator,
@@ -67,14 +57,26 @@ public class GeneticMaxSatSolver extends MaxSatSolver {
 
         wcnfFileContent = String.join("\n", allLines.toArray(new String[0]));
 
+        softWeightCounter();
         fit();
 
         return superSolutions;
     }
 
-    private String changeSoftWeights(int newSoftWeights, String wcnfContent, boolean writeToFile){
-        int top = 0;
-        String[] wcnfContentSplit = wcnfContent.split("\n");
+    public String changeSoftWeights(int[] newSoftWeights, boolean writeToFile){
+        int oldTop = 0;
+        int currentSoftWeightsSum = 0;
+        HashMap<Integer, Integer> oldNewWeights = new HashMap<>();
+
+        for (int i=0; i < softWeightsCounter.size(); i++) {
+            Pair<Integer, Integer> softWeightPair = softWeightsCounter.get(i);
+            oldNewWeights.put(softWeightPair.a, newSoftWeights[i]);
+            softWeightsCounter.remove(softWeightPair);
+            softWeightsCounter.add(new Pair<>(newSoftWeights[i], softWeightPair.b)); // replacing the old weights with new one (count remains the same)
+            currentSoftWeightsSum += newSoftWeights[i]*softWeightPair.b;
+        }
+
+        String[] wcnfContentSplit = this.wcnfFileContent.split("\n");
 
         StringBuilder WCNFModInput = new StringBuilder();
 
@@ -82,10 +84,13 @@ public class GeneticMaxSatSolver extends MaxSatSolver {
 
             String[] trimAndSplit = line.trim().split(" ");
 
+            int weight = Integer.parseInt(trimAndSplit[0]);
+
             if (trimAndSplit[0].equals("p")) {
-                top = Integer.parseInt(trimAndSplit[4]);
-            } else if (top != 0 && Integer.parseInt(trimAndSplit[0]) < top) {
-                trimAndSplit[0] = String.valueOf(newSoftWeights);
+                oldTop = Integer.parseInt(trimAndSplit[4]);
+                trimAndSplit[4] = String.valueOf(currentSoftWeightsSum); // replacing the top value with current sum of soft weights
+            } else if (oldTop != 0 && weight < oldTop) {
+                trimAndSplit[0] = String.valueOf(oldNewWeights.get(weight));
             }
             WCNFModInput.append(String.join(" ", trimAndSplit));
             WCNFModInput.append("\n");
@@ -101,75 +106,37 @@ public class GeneticMaxSatSolver extends MaxSatSolver {
         return WCNFModInput.toString();
     }
 
-    private int fitness(final int[] x) {
-        IPBSolver solver = org.sat4j.maxsat.SolverFactory.newDefault();;
-        WDimacsReader reader = new WDimacsReader(new WeightedMaxSatDecorator(solver));
-        Map<Integer, AnnotationMirror> solutions;
-        int fitness_count = 0;
+    public void softWeightCounter(){
 
-        String WCNFModInput = changeSoftWeights(x[0], wcnfFileContent, false);
+        int top = 0;
+        List<Integer> allSoftWeights = new ArrayList<>();
+        String[] wcnfContentSplit = this.wcnfFileContent.split("\n");
 
-        InputStream stream = new ByteArrayInputStream(WCNFModInput.getBytes(StandardCharsets.UTF_8));
+        for (String line : wcnfContentSplit) {
 
-        try {
-            solver = (IPBSolver) reader.parseInstance(stream);
-        } catch (ContradictionException | IOException | ParseFormatException e) {
-            System.out.println(e);
+            String[] trimAndSplit = line.trim().split(" ");
+            int weightValue = Integer.parseInt(trimAndSplit[0]);
+
+            if (trimAndSplit[0].equals("p")) {
+                top = Integer.parseInt(trimAndSplit[4]);
+            } else if (top != 0 && weightValue < top) {
+                allSoftWeights.add(weightValue);
+            }
         }
 
-        try {
-            if (solver.isSatisfiable()){
-                solutions = decode(solver.model());
+        allSoftWeightsSum = allSoftWeights.stream().mapToLong(Integer::longValue).sum();
+        allSoftWeightsCount = allSoftWeights.size();
+        uniqueSoftWeightsCount = allSoftWeights.stream().distinct().count();
+        oldSoftWeights = allSoftWeights.stream().distinct().collect(Collectors.toList());
 
-                List<AnnotationMirror> sol = new ArrayList<>(solutions.values());
-
-                for (AnnotationMirror sol_0 : sol){
-                    if (sol_0.toString().equals("@universe.qual.Rep"))
-                    {
-                        fitness_count += 1;
-                    }
-                }
-            }
-            else {
-                System.out.println("UNSAT at " + x[0]);
-            }
-        } catch (TimeoutException e) {
-            e.printStackTrace();
+        for (int weight: oldSoftWeights) {
+            softWeightsCounter.add(new Pair<>(weight, Collections.frequency(allSoftWeights, weight)));
         }
 
-        System.out.println("Rep count: " + fitness_count);
-
-        return fitness_count;
     }
 
-    private void fit() {
-        final Engine<IntegerGene, Integer> engine = Engine
-                .builder(
-                        this::fitness,
-                        Codecs.ofVector(IntRange.of(0, 700), 1))
-                .populationSize(500)
-                .offspringFraction(0.7)
-                .survivorsSelector(new RouletteWheelSelector<>())
-                .offspringSelector(new TournamentSelector<>())
-                .optimize(Optimize.MAXIMUM)
-                .alterers(
-                        new Mutator<>(0.03),
-                        new MeanAlterer<>(0.6))
-                .build();
-
-        final EvolutionStatistics<Integer, ?>
-                statistics = EvolutionStatistics.ofNumber();
-
-        final EvolutionResult<IntegerGene, Integer> best_res = engine.stream()
-                .limit(bySteadyFitness(7))
-                .limit(100)
-                .peek(statistics)
-                .collect(toBestEvolutionResult());
-
-        System.out.println(statistics);
-        System.out.println(best_res.genotypes().length());
-        System.out.println(best_res.bestPhenotype());
-        System.out.println(best_res.worstPhenotype());
+    public void fit() {
+        throw new BugInCF("Method needs to be overridden");
     }
 
 }
